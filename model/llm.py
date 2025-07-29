fa8e8b1fb9ca619b9c88d97920d315e5f4a85923
Kushagra-Kataria <165515399+Kushagra-Kataria@users.noreply.github.com>
2025-07-29 15:40:29 +0530

    Deploy

--- model/llm.py
+++ model/llm.py
@@ -9,6 +9,8 @@ LLM_API = os.getenv("LLM_API_KEY")
 OPEN_AI_KEY =os.getenv("OPEN_AI_API_KEY")
 GROQ_API_KEY = os.getenv("GROQ_APO_KEY")
 
+os.environ['GROQ_API_KEY'] = GROQ_API_KEY
+
 OPEN_ROUTER_Model = "deepseek/deepseek-r1-0528:free"
 OPEN_AI_Model = 'gpt-4.1-mini-2025-04-14'
 GROQ_Model = "gemma2-9b-it"
